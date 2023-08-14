1c3fcd72238c06f8e6c4243b15cb3487b853830e
ErdalOzcelik297 <139227815+ErdalOzcelik297@users.noreply.github.com>
2023-08-14 22:00:29 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,14 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
+        <Text variant="h1">Smith`s Takeaway</Text>
         <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
+          This service has been created as a final project forIntelligent Agents and Process Automation (H9IAPI) course to automize Smith`s Takeaway Restaurants order management system.
         </Text>
       </section>
 
       <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+        <Text variant="h2">AI-powered Digital Assistant: Makes it easy for you</Text>
         <div className="lg:w-2/3">
           <Chat />
         </div>
