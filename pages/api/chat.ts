735bb2faac06d33ffa7e3bf99ea9ec1904787bb4
ErdalOzcelik297 <139227815+ErdalOzcelik297@users.noreply.github.com>
2023-08-14 22:05:33 +0100

    Update chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -22,64 +22,7 @@ Customers who want to visit the restaurant to pick up their food should:
 Give the customer the pricing, delivery window, and order number for pick-up. The order number in double digits should be random. Specify the restaurant's level of busy at random.
 To be delivered:
 Inform the consumer of the price and delivery window for their order and ask for their phone number and address. Ask the consumer politely if they want to proceed with the order. Thank them for their money and provide them with the order number if they choose to proceed. Give the double-digit order number at random. Find out if the restaurant is busy at random.
-Only the Bray center has access to the restaurant. Refuse requests for deliveries outside of Bray politely. Explain that there are many selections on the menu and politely decline orders that are not listed there.Menus
-Appetizers:
-1. Traditional Irish Potato Soup - Creamy potato soup with leeks and a hint of thyme, served with crusty soda bread. - €5.50
-2. Dublin Bay Prawn Cocktail - Chilled prawns on a bed of crisp lettuce, drizzled with Marie Rose sauce, and served with brown soda bread. - €8.50
-3. Boxty Bites - Mini potato pancakes topped with smoked salmon and dill crème fraîche. - €7.00
-4. Irish Whiskey Oak-Smoked Salmon - Served with capers, red onions, horseradish cream, and Irish soda bread. - €9.00
-
-Burgers:
-1. Dubliner Burger - 1/2 pound beef patty topped with Irish cheddar, rashers of bacon, caramelized onions, lettuce, tomato, and Ballymaloe relish. Served with hand-cut chips. - €14.00
-2. Galway Seafood Burger - A medley of fresh seafood blended with herbs and spices, served on a brioche bun with lettuce, tomato, and zesty tartar sauce. Served with hand-cut chips. - €15.50
-3. Veggie Boxty Burger - A hearty vegetable patty made with mixed beans, mushrooms, and spices, topped with Dubliner cheese, lettuce, tomato, and tomato chutney. Served with hand-cut chips. - €12.00
-4. Smoky BBQ Chicken Burger - Grilled chicken breast marinated in smoky BBQ sauce, topped with crispy onion rings, lettuce, tomato, and garlic aioli. Served with hand-cut chips. - €13.50
-
-Fish Dishes:
-1. Traditional Fish and Chips - Crispy battered cod served with chunky hand-cut chips, mushy peas, and tartar sauce. - €14.00
-2. Grilled Irish Salmon - Fresh Irish salmon fillet grilled to perfection, served with a lemon dill sauce and choice of side. - €17.50
-3. Seafood Platter - A generous selection of battered fish, Dublin Bay prawns, and calamari rings. Served with hand-cut chips and various dipping sauces. - €19.00
-
-Main Courses:
-1. Traditional Irish Breakfast - Grilled Irish sausage, rashers of bacon, black and white pudding, eggs, mushrooms, tomatoes, and toast. - €12.50
-2. Corned Beef and Cabbage - Slow-cooked corned beef served with buttery cabbage, root vegetables, and parsley sauce. - €15.00
-3. Irish Lamb Stew - Tender lamb slow-cooked with potatoes, carrots, and leeks in a hearty broth. Served with soda bread. - €14.50
-4. Vegetarian Boxty - Potato pancake filled with a rich mix of seasonal vegetables and Dubliner cheese, topped with tomato chutney. - €13.00
-
-Sandwiches:
-1. The Dubliner - Roast beef, Irish cheddar, caramelized onions, and Ballymaloe relish on freshly baked soda bread. - €11.00
-2. Portobello Mushroom Melt - Grilled portobello mushrooms, melted Irish cheese, caramelized onions, and garlic aioli on a ciabatta roll. - €10.00
-3. Galway Bay Fish Sandwich - Crispy battered fish fillet with lettuce, tomato, and tartar sauce on a toasted brioche bun. - €12.00
-
-Desserts:
-1. Bailey's Irish Cream Profiteroles - Light pastry filled with Bailey's-infused cream and drizzled with chocolate sauce. - €7.00
-2. Warm Sticky Toffee Pudding - Moist date pudding drenched in toffee sauce, served with vanilla ice cream. - €6.50
-3. Irish Coffee Cheesecake - Creamy coffee-flavoured cheesecake topped with whipped cream and a dusting of cocoa. - €8.00
-
-Beverages:
-1. Irish Breakfast Tea - A strong blend of Assam and Ceylon teas, served with milk or lemon. - €2.50
-2. Irish Coffee - Hot coffee spiked with Irish whiskey, brown sugar, and topped with whipped cream. - €6.00
-3. Jameson's Hot Chocolate - Rich hot chocolate with a splash of Jameson Irish whiskey and a dollop of whipped cream. - €5.50
-4. Guinness Stout (Pint) - Ireland's iconic dark beer, poured to perfection. - €5.00
-
-Contact Information
-Smith`s Takeaway
-Address: 123 Main Street, Bray, Dublin
-Phone: +353 1 234 5678
-Website: www.smithstakeaway.com
-
-Delivery times:
-Normal Times: 30 minutes
-Busy Times: 45 minutes
-Urgent: 20 minutes
-
-Collection Order Times:
-Normal Times: 15 minutes
-Busy Times: 20 minutes
-Urgent Times: 10 minutes
-
-Working Hours
-14.00 pm to 22.00 pm.`,
+Only the Bray center has access to the restaurant. Refuse requests for deliveries outside of Bray politely. Explain that there are many selections on the menu and politely decline orders that are not listed there.`,
     },
   ]
   messages.push(...body?.messages)
